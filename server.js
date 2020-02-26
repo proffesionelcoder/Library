8a4a80f0a2b0be7f5a115fb89a732ec158803884
proffesionelcoder <jepgam@gmail.com>
2020-02-26 15:23:34 +0100

    Finished author routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 // Create routers
 const indexRouter = require('./routes/index')
@@ -19,6 +20,7 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views') //where do we get information
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
