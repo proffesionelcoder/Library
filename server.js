cb21bbcf119f24df2f0eac6f51bb242f03a767e6
proffesionelcoder <jepgam@gmail.com>
2020-02-26 13:25:11 +0100

    Initial setup

---
+++ server.js
@@ -0,0 +1,37 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+
+// Download the different libraries
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
+// Create routers
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
+//Set up the view-engine
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views') //where do we get information
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+
+// Set up mongoDB
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+// Tell the app, what routes to use with differen url's
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+
+app.listen(process.env.PORT || 3000)
